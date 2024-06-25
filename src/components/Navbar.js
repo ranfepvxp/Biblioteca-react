19a7d2ae1f93ab4e04de209e2e0b3815b0070006
Ranferi <ranfepv@gmail.com>
2024-06-25 14:22:27 -0600

    Comenzando a trabajar con el navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,12 +11,11 @@ function Navbar()
     return (
         <div className="container">
         <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-            <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-        <span class="fs-4">
+            <span class="fs-4">
             {
             user!=null ? `Bienvenido ${user}!` :  <LoginButton login={() => setComponentState('login') } />
     } {componentState} </span>
-      </a>
+      
 
       <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
         <a class="me-3 py-2 text-dark text-decoration-none" href="#">Features</a>
